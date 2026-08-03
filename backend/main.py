24ecf140d0b9c8027d74c5199b940e02d0c27d2e
medarametla-kalesh123 <medarametlakalesh@gmail.com>
2026-08-03 13:05:53 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -92,21 +92,15 @@ app = FastAPI(
 # ================= CORS =================
 
 app.add_middleware(
-
     CORSMiddleware,
-
     allow_origins=[
-        "http://localhost:3000"
+        "http://localhost:3000",
+        "https://mirchi1.netlify.app"
     ],
-
     allow_credentials=True,
-
     allow_methods=["*"],
-
     allow_headers=["*"]
-
 )
-
 
 # ================= Home API =================
 
